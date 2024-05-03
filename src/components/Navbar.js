f624d2b39eef27b2b9cbb96e8cadcfaaec8ad88f
Justin Signo <justinsigno@yahoo.com>
2024-05-03 18:35:34 -0400

    figure out page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,11 +27,6 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <Link to="work" smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li>
           <Link to="contact" smooth={true} duration={500}>
             Contact
           </Link>
@@ -60,12 +55,6 @@ const Navbar = () => {
           {" "}
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          {" "}
-          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
-            Work
           </Link>
         </li>
         <li className="py-6 text-4xl">
